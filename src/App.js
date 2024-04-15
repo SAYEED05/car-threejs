19b397fa42b8e35b9b5a3acb404cc8c2670cf597
sayeedzh <sayeed.705@gmail.com>
2024-04-15 15:51:53 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Stats } from "@react-three/drei";
 import "./App.css";
 import { Ground } from "./Ground";
+import { Car } from "./Car";
 
 function CarShow() {
   return (
     <>
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      {/* <directionalLight position={[10, 20, 5]} intensity={20} /> */}
       <color args={[0, 0, 0]} attach="background" />
-      <spotLight
+      {/* <spotLight
         color={[1, 0.25, 0.7]}
         intensity={150}
         angle={0.6}
@@ -27,7 +29,26 @@ function CarShow() {
         position={[-5, 5, 0]}
         castShadow
         shadow-bias={-0.0001}
+      /> */}
+      <spotLight
+        color={[10, 10, 10]}
+        intensity={300}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 15, 0]}
+        castShadow
+        shadow-bias={-0.0001}
       />
+      <spotLight
+        color={[10, 10, 10]}
+        intensity={300}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-35, 15, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <Car />
       <Ground />
     </>
   );
@@ -38,6 +59,7 @@ function App() {
     <Suspense fallback={null}>
       <Canvas shadows>
         <CarShow />
+        <Stats />
       </Canvas>
     </Suspense>
   );
---
+++ src/Car.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Mesh, MeshBasicMaterial } from "three";
+
+// based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a)
+// by Martin Trafas (https://sketchfab.com/Bexxie) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+export function Car() {
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "models/car/scene.gltf"
+  );
+
+  const wheelRotation = 4;
+
+  console.log(gltf, "gltf");
+
+  useEffect(() => {
+    gltf.scene.scale.set(0.005, 0.005, 0.005);
+    gltf.scene.position.set(0, -0.035, 0);
+    gltf.scene.traverse((object) => {
+      console.log(object, "object");
+      if (object instanceof Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+
+  // useFrame((state, delta) => {
+  //   let t = state.clock.getElapsedTime();
+
+  //   let group = gltf.scene.children[0].children[0].children[0];
+  //   group.children[0].rotation.x = t * wheelRotation;
+  //   group.children[2].rotation.x = t * wheelRotation;
+  //   group.children[4].rotation.x = t * wheelRotation;
+  //   group.children[6].rotation.x = t * wheelRotation;
+  // });
+
+  return <primitive object={gltf.scene} />;
+}
